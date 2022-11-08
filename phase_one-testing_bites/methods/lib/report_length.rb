8a333f3563dec41a2e0e17c24c471abe8db91a84
williamjones <willj836@gmail.com>
2022-11-08 21:20:47 +0000

    added test for error

--- phase_one-testing_bites/methods/lib/report_length.rb
+++ phase_one-testing_bites/methods/lib/report_length.rb
@@ -1,7 +1,5 @@
 def report_length(str)
-  raise ArgumentError.new(
-    "Expected argument to be a string"
-  ) if !str.is_a? String
+  raise "Expected argument to be a string" if !str.is_a? String
   length = str.length
   return "This string was #{length} characters long."
 end
--- phase_one-testing_bites/methods/spec/report_length_spec.rb
+++ phase_one-testing_bites/methods/spec/report_length_spec.rb
@@ -10,4 +10,8 @@ describe 'report_length method to return lengths of string' do
     expected = "This string was 11 characters long."
     expect(report_length('darth vader')).to eq(expected)
   end 
+
+  it "raises error if not string argument" do 
+    expect { report_length(123) }.to raise_error "Expected argument to be a string"
+  end
 end 
