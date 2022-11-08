a41a8f5fa90037703b4e440e23c3e08bb4116b45
williamjones <willj836@gmail.com>
2022-11-08 16:55:50 +0000

    rubocopped

--- phase_two-skill_challenges/design_single_method/exercise1/lib/exercise1.rb
+++ phase_two-skill_challenges/design_single_method/exercise1/lib/exercise1.rb
@@ -1,4 +1,4 @@
-def calculate_reading_time text 
+def calculate_reading_time(text)
   array = text.split(' ')
   time = (array.length / 200.to_f).ceil
   "#{time} #{time == 1 ? 'minute' : 'minutes'}"
--- phase_two-skill_challenges/design_single_method/exercise2/lib/exercise2.rb
+++ phase_two-skill_challenges/design_single_method/exercise2/lib/exercise2.rb
@@ -1,5 +1,8 @@
-def check_grammar text
-  fail 'Not even a string.' if !text.is_a? String
-  fail 'Not a sentence.' if text.empty?
+# frozen_string_literal: true
+
+def check_grammar(text)
+  raise 'Not even a string.' unless text.is_a? String
+  raise 'Not a sentence.' if text.empty?
+
   text[0].upcase == text[0] and text[-1] =~ /[.?!]/ ? true : false
 end
