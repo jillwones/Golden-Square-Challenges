e166c1e1f4a3a98e643e0d96531dc45cd9370704
williamjones <willj836@gmail.com>
2022-11-08 16:52:38 +0000

    corrected error in ex 2

--- phase_two-skill_challenges/design_single_method/exercise2/lib/exercise2.rb
+++ phase_two-skill_challenges/design_single_method/exercise2/lib/exercise2.rb
@@ -1,5 +1,5 @@
 def check_grammar text
   fail 'Not even a string.' if !text.is_a? String
-  raise ArgumentError.new 'Not a sentence.' if text.empty?
+  fail 'Not a sentence.' if text.empty?
   text[0].upcase == text[0] and text[-1] =~ /[.?!]/ ? true : false
 end
