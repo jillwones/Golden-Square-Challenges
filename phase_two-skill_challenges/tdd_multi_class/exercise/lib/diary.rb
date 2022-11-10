5256349c2d92c03f43c9184f3108ff4705088722
williamjones <willj836@gmail.com>
2022-11-10 16:50:36 +0000

    pass most intergration tests
    will now build out unit tests before final feature test

--- phase_two-skill_challenges/tdd_multi_class/exercise/lib/diary.rb
+++ phase_two-skill_challenges/tdd_multi_class/exercise/lib/diary.rb
@@ -10,4 +10,21 @@ class Diary
   def all
     @diary
   end
+  
+  def count_words
+    sum = 0
+    @diary.each do |entry|
+      sum += entry.count_words
+    end 
+    sum
+  end
+
+  def reading_time wpm 
+    sum = 0
+    @diary.each do |entry|
+      sum += entry.reading_time_float(wpm)
+    end 
+    sum.ceil
+  end
+
 end
--- phase_two-skill_challenges/tdd_multi_class/exercise/lib/diary_entry.rb
+++ phase_two-skill_challenges/tdd_multi_class/exercise/lib/diary_entry.rb
@@ -3,4 +3,17 @@ class DiaryEntry
     @title = title
     @contents = contents
   end
+
+  def count_words 
+    @contents.split(' ').count
+  end
+
+  def reading_time_float(wpm) 
+    (count_words / wpm.to_f) 
+  end
+
+  def reading_time_int(wpm) 
+    (count_words / wpm.to_f).ceil 
+  end
+
 end
