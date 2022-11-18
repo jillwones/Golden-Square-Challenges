74b8b2bebda8caac68e74ebff50541176decdae5
williamjones <willj836@gmail.com>
2022-11-18 11:16:48 +0000

    shows 2 example menus in run.rb

--- 04_solo_project/lib/run.rb
+++ 04_solo_project/lib/run.rb
@@ -3,15 +3,20 @@ require_relative './Menu'
 require_relative './Order'
 require_relative './Text'
 
+# ---------------------------------- RUN AS DEFAULT ---------------------------------
+
+# burger restaurant is the default so when nothing is passed to Menu.new the menu is a burger menu
+
 burger_restaurant = Menu.new
 burger_restaurant.list_available 
-=begin
-Items in stock:
-1. burger - £4.50
-2. hot dog - £3.00
-3. CocaCola - £1.00
-4. chips - £2.00
-=end
+# items in stock are shown in the terminal:
+
+# Items in stock:
+# 1. burger - £4.50
+# 2. hot dog - £3.00
+# 3. CocaCola - £1.00
+# 4. chips - £2.00
+
 my_order = Order.new(burger_restaurant)
 my_order.add('burger', 6)
 my_order.add('CocaCola', 2)
@@ -27,12 +32,36 @@ What you ordered:
 1x chips - £2.00
 Grand Total: £31.00
 =end
-# sends a text to the default number (mine) with a confirmation, if I ordered at 7:05pm the text will say '"Thank you! Your order was placed and will be delivered before 7:25 PM'
+# then a text is snet to the default number (mine) with a confirmation, if I ordered at 7:05pm the text will say '"Thank you! Your order was placed and will be delivered before 7:25 PM'
 
-# TODOs:
+# -------------------------- RUN WITH DIFFERENT MENU --------------------------------
 
-# add number of items as an argument to add method in Order => done
-# google => mock twilio gem!!!
-# configure rubocop so rspec isnt ugly anymore
-# pass menu in initialize in order => done
-# make more modular, so have a method in menu that decreases the stock count, order shouldnt fiddle with the menu instance => done
+chinese_restaurant = Menu.new(Kernel, [
+  { name: 'noodles', price: 7.00, quantity: 10 },
+  { name: 'satay chicken', price: 3.50, quantity: 10 },
+  { name: 'egg fried rice', price: 6.00, quantity: 5 },
+  { name: 'roasted duck', price: 9.00, quantity: 5 }
+])
+chinese_restaurant.list_available
+# shows items in stock
+
+# Items in stock:
+# 1. noodles - £7.00
+# 2. satay chicken - £3.50
+# 3. egg fried rice - £6.00
+# 4. roasted duck - £9.00
+
+my_order = Order.new(chinese_restaurant)
+my_order.add('noodles', 6)
+my_order.add('roasted duck', 3)
+my_order.add('satay chicken', 2)
+my_order.complete_order
+# first the receipt is shown:
+
+# What you ordered:
+# 6x noodles - £42.00
+# 3x roasted duck - £27.00
+# 2x satay chicken - £7.00
+# Grand Total: £76.00
+
+# then the confirmation text is sent
